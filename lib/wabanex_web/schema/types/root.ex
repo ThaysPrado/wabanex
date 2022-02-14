b56f9e58f189ed4cd68d3f45c6134bb532364e8f
ThaysPrado <thaysprado96@gmail.com>
2022-02-14 20:39:07 -0300

    create user with mutatio

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -14,4 +14,12 @@ defmodule WabanexWeb.Schema.Types.Root do
       # resolve fn params, context -> UserResolver.get(params, context) end
     end
   end
+
+  object :root_mutation do
+    field :create_user, type: :user do
+      arg(:input, non_null(:create_user_input))
+
+      resolve(&UserResolver.create/2)
+    end
+  end
 end
